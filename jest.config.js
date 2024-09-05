194178c377067f133bed55453ffc6edf7c55569e
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2024-09-05 19:04:09 +0400

    Unit test initial setup. dateUtils tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: [ 'ts', 'js' ],
+  testMatch: [ '**/*.test.ts' ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
---
+++ src/__tests__/dateUtils.test.ts
@@ -0,0 +1,36 @@
+import { describe, expect, test } from '@jest/globals';
+import { dateUtils } from "../utils/utils/dateUtils";
+import { NoDateFoundError } from "../utils/errors/errors";
+
+describe("Date utils", () => {
+  test("isInFuture returns true when date is in future", () => {
+    const date = new Date("12-06-2055");
+    expect(dateUtils.isInFuture(date)).toBe(true);
+  });
+
+  test("isInFuture returns false when date is NOT in future", () => {
+    const date = new Date("12-06-2015");
+    expect(dateUtils.isInFuture(date)).toBe(false);
+  });
+
+  test("getDay retrieves day from string", () => {
+    const word: string = "23-ին";
+    expect(dateUtils.getDay(word)).toBe(23);
+  });
+
+  test("getDay returns undefined when no number is present in string", () => {
+    const word: string = "test";
+    expect(dateUtils.getDay(word)).toBe(undefined);
+  });
+
+  test("getDate throws NoDateFoundError when no date is found", () => {
+    const text: string = "«Հայաստանի էլեկտրական ցանցեր» փակ բաժնետիրական " +
+      "ընկերությունը տեղեկացնում է, որ պլանային նորոգման " +
+      "աշխատանքներ իրականացնելու նպատակով ժամանակավորապես կդադարեցվի " +
+      "հետևյալ հասցեների էլեկտրամատակարարումը`";
+
+    expect(() => {
+      dateUtils.getDate(text);
+    }).toThrow(NoDateFoundError);
+  });
+});
